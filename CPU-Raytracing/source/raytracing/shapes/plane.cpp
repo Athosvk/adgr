611a9c8ead36987601acf75dc402256f1ff0fb29
Athos van Kralingen <athos.van.kralingen@hva.nl>
2021-11-28 12:03:29 +0100

    Fixed backface check on plane intersection, added intersection normal in result

--- CPU-Raytracing/source/raytracing/shapes/plane.cpp
+++ CPU-Raytracing/source/raytracing/shapes/plane.cpp
@@ -5,16 +5,26 @@ namespace CRT
 	Plane::Plane(float3 _point, float3 _normal)
 		: Shape(ShapeType::SHAPE_TYPE_PLANE)
 		, Point(_point)
-		, Normal(-_normal) // TODO:: Look into this minus...
+		, Normal(_normal) 
 	{ }
 
 	bool Plane::Intersect(Ray _r, Manifest& _m)
 	{
-		float t = Normal.Dot(_r.D);
-		if (t > 1e-6) {
+		float cosTheta = Normal.Dot(_r.D);
+		// Backface check; if plane normal faces opposite direction of 
+		// ray direction, the normal is facing the ray so it's a front face.
+		if (-cosTheta > 1e-6)
+		{
 			float3 p0l0 = Point - _r.O;
-			t = p0l0.Dot(Normal) / t;
-			return (t >= 0.0f);
+			float t = p0l0.Dot(Normal) / cosTheta;
+
+			if (t >= 0.0f)
+			{
+				_m.T = t;
+				_m.N = Normal;
+				return true;
+			}
+			return false;
 		}
 		return false;
 	}
