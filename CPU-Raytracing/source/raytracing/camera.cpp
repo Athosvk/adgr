54e3bcd57171d30f09f737084e43c28034feb97e
Athos van Kralingen <athos.van.kralingen@hva.nl>
2021-11-29 20:54:30 +0100

    Fixed translation/rotation

--- CPU-Raytracing/source/raytracing/camera.cpp
+++ CPU-Raytracing/source/raytracing/camera.cpp
@@ -9,8 +9,13 @@ namespace CRT
 	{
 		return glm::vec3(_value.x, _value.y, _value.z);
 	}
+
 	Camera::Camera(float2 _viewportSize) :
-		m_ViewportSize(_viewportSize)
+		m_ViewportSize(_viewportSize),
+		m_Front(float3::Forward()),
+		m_Up(float3::Up()),
+		m_Right(float3::Right()),
+		m_View(ConstructView())
 	{
 	}
 
@@ -27,7 +32,7 @@ namespace CRT
 	Ray Camera::ConstructRay(float2 _uv) const
 	{
 		float3 focalDirection(0.0, 0.0, -1.0);
-		float3 cameraPlane = (m_FocalLength * focalDirection) + m_Position;
+		float3 cameraPlane = (m_FocalLength * focalDirection);
 		float3 p0 = cameraPlane + float3(-1, 1, 0);
 		float3 p1 = cameraPlane + float3(1, 1, 0);
 		float3 p2 = cameraPlane + float3(-1, -1, 0);
@@ -35,13 +40,16 @@ namespace CRT
 		float3 uv = p0 + (_uv.x * (p1 - p0)) + (_uv.y * (p2 - p0));
 		float aspect_ratio = m_ViewportSize.x / (float)m_ViewportSize.y;
 		uv.x *= aspect_ratio;
-		float3 d = Transform((uv - m_Position).Normalize(), ConstructView());
-		return Ray(m_Position, d);
+		float3 d = (Transform((uv), m_View)).Normalize();
+		return Ray(m_Position, d.Normalize());
 	}
 
 	void Camera::SetDirection(float3 _direction)
 	{
 		m_Front = _direction;
+		m_Right = m_Front.Cross(float3::Up()).Normalize();
+		m_Up = m_Right.Cross(m_Front).Normalize();
+		m_View = ConstructView();
 	}
 
 	float3 Camera::GetFront() const
@@ -51,19 +59,29 @@ namespace CRT
 
 	float3 Camera::GetUp() const
 	{
-		return float3::Up();
+		return m_Up;
 	}
 
 	float3 Camera::GetRight() const
 	{
-		return m_Front.Cross(float3::Up()).Normalize();
+		return m_Right;
+	}
+
+	float Camera::GetFocalLength() const
+	{
+		return m_FocalLength;
+	}
+
+	void Camera::SetFocalLength(float focalLength)
+	{
+		m_FocalLength = focalLength;
 	}
 
 	glm::mat4 Camera::ConstructView() const
 	{
-		glm::vec3 position = ToGlm(m_Position);
+		glm::vec3 position = ToGlm(float3::Zero());
 		glm::vec3 front = ToGlm(m_Front);
-		return glm::lookAt(position, position + front, ToGlm(GetUp()));
+		return glm::inverse(glm::lookAt(position, position + front, ToGlm(float3::Up())));
 	}
 
 	float3 Camera::Transform(float3 _toTransform, glm::mat4 _transform) const
--- CPU-Raytracing/source/raytracing/camera.h
+++ CPU-Raytracing/source/raytracing/camera.h
@@ -19,13 +19,18 @@ namespace CRT
 		float3 GetFront() const;
 		float3 GetUp() const;
 		float3 GetRight() const;
+		float GetFocalLength() const;
+		void SetFocalLength(float focalLength);
 	private:
 		glm::mat4 ConstructView() const;
 		float3 Transform(float3 _toTranform, glm::mat4 _transform) const;
 
-		float m_FocalLength = 1.0f;
+		float m_FocalLength = 3.0f;
 		float2 m_ViewportSize;
 		float3 m_Position = { 0.0f, 0.0f, 0.0f };
-		float3 m_Front = { 0.0f, 0.0f, -1.0f };
+		float3 m_Front;
+		float3 m_Up;
+		float3 m_Right;
+		glm::mat4 m_View;
 	};
 }
