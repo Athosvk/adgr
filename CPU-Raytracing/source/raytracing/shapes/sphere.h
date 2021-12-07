c8ca58a078e8e9e17b40283b18d29d19bf2856b6
Athos van Kralingen <athos.van.kralingen@hva.nl>
2021-12-07 19:14:27 +0100

    Added sphere intersection for when inside sphere

--- CPU-Raytracing/source/raytracing/shapes/sphere.h
+++ CPU-Raytracing/source/raytracing/shapes/sphere.h
@@ -9,8 +9,9 @@ namespace CRT
 		Sphere(float3 _position, float _radius);
 
 		virtual bool Intersect(Ray _r, Manifest& _m) override;
-
 	private:
+		bool IntersectOuter(Ray _r, Manifest& _m);
+		bool IntersectInner(Ray _r, Manifest& _m);
 		virtual float2 GetUV(float3 _point, float3 _normal) override;
 
 		float  Radius2;
