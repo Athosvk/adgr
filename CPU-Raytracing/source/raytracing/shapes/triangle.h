ea3694f564c4b5d4b6e3429561fcba4655b46f17
Pepijn Averink <45821784+PepijnAverink@users.noreply.github.com>
2021-12-07 22:43:26 +0100

    Update triangle.h

--- CPU-Raytracing/source/raytracing/shapes/triangle.h
+++ CPU-Raytracing/source/raytracing/shapes/triangle.h
@@ -7,13 +7,13 @@ namespace CRT
 	{
 	public:
 		Triangle();
-		Triangle(float3 _v0, float3 _v1, float3 _v2, float3 _n);
+		Triangle(float3 _v0, float3 _v1, float3 _v2, float2 u0, float2 _u1, float2 _u2, float3 _n);
 
 		virtual bool Intersect(Ray _r, Manifest& _m) override;
 
-		float3 v0;
-		float3 v1;
-		float3 v2;
+		float3 V0;
+		float3 V1;
+		float3 V2;
 
 		float2 u0;
 		float2 u1;
