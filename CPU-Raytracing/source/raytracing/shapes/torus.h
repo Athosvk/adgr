2915313a79c79406082628443854114cb46e3419
Pepijn Averink <45821784+PepijnAverink@users.noreply.github.com>
2021-12-08 10:27:38 +0100

    Update torus.h

--- CPU-Raytracing/source/raytracing/shapes/torus.h
+++ CPU-Raytracing/source/raytracing/shapes/torus.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "./raytracing/shapes/shape.h"
+#include <glm/mat4x4.hpp>
 
 namespace CRT
 {
 	class Torus final : public Shape
 	{
 	public:
-		Torus(float _r1, float _r2);
+		Torus(float3 _pos, float _r1, float _r2);
 
 		virtual bool Intersect(Ray _r, Manifest& _m) override;
 
 	private:
 		virtual float2 GetUV(float3 _point, float3 _normal) override;
+
+		float3 Position;
+		glm::mat4x4 invModel;
+		glm::mat4x4 model;
 
 		float RR1;
 		float RR2;
