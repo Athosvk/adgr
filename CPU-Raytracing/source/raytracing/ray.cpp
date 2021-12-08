0a60b2f2c86e03a9e643cf4efd23ca6b9a544cf4
Pepijn Averink <45821784+PepijnAverink@users.noreply.github.com>
2021-12-08 10:27:18 +0100

    Update ray.cpp

--- CPU-Raytracing/source/raytracing/ray.cpp
+++ CPU-Raytracing/source/raytracing/ray.cpp
@@ -8,6 +8,21 @@ namespace CRT
 		return Ray(O, r);
 	}
 
+	Ray Ray::Transform(glm::mat4x4& _matrix)
+	{
+		glm::vec4 origin(O.x, O.y, O.z, 1.0f);
+		glm::vec4 dir(D.x, D.y, D.z, 0.0f);
+		glm::vec4 whatever(1.0f, 2.0f, 3.0f, 4.0f);
+
+		glm::vec3 n0 = origin*_matrix;
+		glm::vec3 d0 = dir*_matrix;
+		glm::vec4 w0 = whatever * _matrix;
+
+		d0 = glm::normalize(d0);
+
+		return Ray(float3(n0.x, n0.y, n0.z), float3(d0.x, d0.y, d0.z));
+	}
+
 	float3 Ray::Sample(float _t) const
 	{
 		return O + D * _t;
